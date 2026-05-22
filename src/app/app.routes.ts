3b490ca9d23734476eb2c44129a19cb77c63c391
carolineaire <noblet.caroline.pro@gmail.com>
2026-05-22 13:28:30 +0200

    feat(player): implement video player component with dynamic video loading and display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,6 @@ export const routes: Routes = [
     {path: '', loadChildren: () => import('./pages/main/main.routes').then(m => m.MAIN_ROUTES)},
     {path: 'auth', loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)},
     {path: 'profile', canActivateChild: [authGuard], loadChildren: () => import('./pages/users/user.routes').then(m => m.USER_ROUTES)},
-    {path: 'player', canActivateChild: [authGuard], component: Player },
+    {path: 'player/:id', canActivateChild: [authGuard], component: Player },
     {path: '**', redirectTo: ''}
 ];
--- src/app/pages/player/player.ts
+++ src/app/pages/player/player.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { VideosS } from '../../services/videos-s';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { YoutubeVideoModel } from '../../models/videosT';
 
 @Component({
   selector: 'app-player',
@@ -6,4 +10,32 @@ import { Component } from '@angular/core';
   templateUrl: './player.html',
   styleUrl: './player.css',
 })
-export class Player {}
+export class Player implements OnInit {
+  video = signal<YoutubeVideoModel | null>(null);
+  videoUrl = signal<SafeResourceUrl | null>(null);
+
+  private readonly params: ActivatedRoute = inject(ActivatedRoute)
+  private readonly videosService = inject(VideosS);
+  private readonly sanitizer = inject(DomSanitizer);
+
+  ngOnInit() {
+
+    const id = this.params.snapshot.queryParams['id'];
+
+    if (!id) return;
+
+    this.videosService.getVideoById(id).subscribe(video => {
+
+      if (video) {
+
+        this.video.set(video);
+
+        const url = `https://www.youtube.com/embed/${video.id}`;
+
+        this.videoUrl.set(
+          this.sanitizer.bypassSecurityTrustResourceUrl(url)
+        );
+      }
+    });
+  }
+}
